ecee39942d2db1b5bd5919bb4b5ed153d408dbae
Alexander Mottram <alexanderdmottram@outlook.com>
2025-12-07 13:13:46 +0000

    Minor updates to demos to make them look nice.

--- guis/demos/multiple_logger_demo.cpp
+++ guis/demos/multiple_logger_demo.cpp
@@ -2,20 +2,17 @@
 
 int main(int, char**)
 {
-    // GUIManager::instance().start(); // no longer required; logger registration will start GUI lazily
-
-    // create three GuiLogger objects and keep shared_ptrs to write to them
+    const int loop_delay_ms {100};
 
     guis::GuiLogger logger_three("mod_3", "Multiples of 3");
     guis::GuiLogger logger_seven("mod_7", "Multiples of 7");
     guis::GuiLogger logger_thirteen("mod_13", "Multiples of 13");
 
     for (int current = 1; current <= 100; ++current) {
-        // std::this_thread::sleep_for(std::chrono::milliseconds(200));
         if (current % 3 == 0) logger_three.append(std::format("Hit {}", current));
         if (current % 7 == 0) logger_seven.append(std::format("Hit {}", current));
         if (current % 13 == 0) logger_thirteen.append(std::format("Hit {}", current));
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(loop_delay_ms));
     }
 
     // give GUI a moment to display
